cd256ba27b387fb468aa349a0c8b9ceae774d437
João José <joaojose@Joaos-MacBook-Pro.local>
2024-02-01 10:06:17 -0300

    feature: queue

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,17 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-         self._data = list()
+        # self._data = list()
+        self._data = []
 
     def __len__(self):
         return len(self._data)
 
     def is_empty(self):
         return self._data == []
-    
+
     def enqueue(self, value):
-         self._data.append(value)
+        self._data.append(value)
 
     def dequeue(self):
         if not self.is_empty():
@@ -21,7 +22,11 @@ class Queue(AbstractQueue):
             return None
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if 0 <= index < len(self._data):
+            return self._data[index]
+        else:
+            raise IndexError("Índice Inválido ou Inexistente")
+
 
 if __name__ == "__main__":
     my_queue = Queue()
