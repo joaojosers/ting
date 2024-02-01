3b773156589d89eb6e8b0904056f88df8c5fff0e
João José <joaojose@Joaos-MacBook-Pro.local>
2024-02-01 09:08:36 -0300

    commencing ting project

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,25 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+         self._data = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._data)
 
+    def is_empty(self):
+        return self._data == []
+    
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+         self._data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if not self.is_empty():
+            return self._data.pop(0)
+        else:
+            return None
 
     def search(self, index):
         """Aqui irá sua implementação"""
+
+if __name__ == "__main__":
+    my_queue = Queue()
