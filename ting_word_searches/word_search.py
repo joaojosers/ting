caace5d21e7b978fc06f165809be30ae6bfd8b37
João José <joaojose@Joaos-MacBook-Pro.local>
2024-02-01 20:34:51 -0300

    feature: function search_by_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -29,5 +29,32 @@ def exists_word(word: str, instance: Queue) -> List[dict]:
     return results
 
 
-def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+# def search_by_word(word, instance):
+#     """Aqui irá sua implementação"""
+
+
+def search_by_word(word: str, instance: Queue) -> List[dict]:
+    results = []
+
+    for i in range(len(instance)):
+        file_info = instance.search(i)
+
+        # Caso a palavra seja encontrada no arquivo, armazena as informações
+        occurrences = []
+        for line_num, line_content in enumerate(
+            file_info["linhas_do_arquivo"], start=1
+        ):
+            if word.lower() in line_content.lower():
+                occurrences.append(
+                    {"linha": line_num, "conteudo": line_content}
+                )
+
+        if occurrences:
+            result_entry = {
+                "palavra": word,
+                "arquivo": file_info["nome_do_arquivo"],
+                "ocorrencias": occurrences,
+            }
+            results.append(result_entry)
+
+    return results
